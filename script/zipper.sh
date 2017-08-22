ebf7bc03a8b66607bd850a2aaa9ffce33dd469df
jianlins <jianlinshi.cn@gmail.com>
2017-08-21 21:01:13 -0600

    add error detection, zip sh

--- script/zipper.sh
+++ script/zipper.sh
@@ -11,15 +11,15 @@ jdkbase=$base/../../../Downloads/jdks
 cd target/deploy
 sed -i 's/jdk1.8\/bin\/java/java/g' run_gui
 sed -i 's/jdk1.8\\bin\\java/java/g' run_gui.bat
-zip -r "../release/EasyCIE_$version_wo_jdk.zip" "./"
+zip -r "../release/EasyCIE_${version}_wo_jdk.zip" "./"
 sed -i 's/java/jdk1.8\/bin\/java/g' run_gui
 sed -i 's/java/jdk1.8\\bin\\java/g' run_gui.bat
-zip -r "../release/EasyCIE_$version_win_jdk.zip" "./"
-zip -r "../release/EasyCIE_$version_mac_jdk.zip" "./"
-zip -r "../release/EasyCIE_$version_linux_jdk.zip" "./"
+zip -r "../release/EasyCIE_${version}_win_jdk.zip" "./"
+zip -r "../release/EasyCIE_${version}_mac_jdk.zip" "./"
+zip -r "../release/EasyCIE_${version}_linux_jdk.zip" "./"
 cd $jdkbase/linux/
-zip -ur "$base/target/release/EasyCIE_$version_linux_jdk.zip" "./"
+zip -ur "$base/target/release/EasyCIE_${version}_linux_jdk.zip" "./"
 cd $jdkbase/win/
-zip -ur "$base/target/release/EasyCIE_$version_win_jdk.zip" "./"
+zip -ur "$base/target/release/EasyCIE_${version}_win_jdk.zip" "./"
 cd $jdkbase/mac/
-zip -ur "$base/target/release/EasyCIE_$version_mac_jdk.zip" "./"
+zip -ur "$base/target/release/EasyCIE_${version}_mac_jdk.zip" "./"
