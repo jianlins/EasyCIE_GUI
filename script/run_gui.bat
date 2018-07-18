8a098ae0749aad6b9699256506e77a394e8d3eb8
jianlins <jianlinshi.cn@gmail.com>
2018-07-18 09:14:19 -0600

    upgrade script

--- script/run_gui.bat
+++ script/run_gui.bat
@@ -1,16 +1,6 @@
 @ECHO OFF
-
 cd /d %~dp0
-
 Set StartInDirectory=%CD%
-
-
 setLocal EnableDelayedExpansion
-set CLASSPATH="
-for /R ./lib %%a in (*.jar) do (
-   set CLASSPATH=!CLASSPATH!;%%a
-)
-set CLASSPATH=!CLASSPATH!"
+${java.win.path} -cp "lib/*;./classes" edu.utah.bmi.simple.gui.controller.Main
 
-
-${java.win.path} -cp ./classes;!CLASSPATH! edu.utah.bmi.simple.gui.controller.Main
