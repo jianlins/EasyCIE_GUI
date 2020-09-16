987fed355c9d5142408ec39df8b8730239dc3097
jianlins <jianlinshi.cn@gmail.com>
2020-09-16 14:54:35 -0600

    v3.0.3

--- script/compile_jfx.sh
+++ script/compile_jfx.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-module load maven3
-module load jdk/jdk1.8
+source ~/.bash_aliases
+# assume .bash_aliases contain scripts to load the modules or set up the PATH.
+#module load maven3
+#module load jdk/jdk1.8
 mvn jfx:build-jar
